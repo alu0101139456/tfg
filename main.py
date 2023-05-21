9659326de3f1130099b95eead92d55939342b9a6
Ángel Julián Bolaño Campos <alu0101139456@ull.edu.es>
2023-05-21 11:53:48 +0100

    fix: Added delete_files if error

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from termcolor import colored
+import shutil
 
 
 def makeGraphic(df,path): 
@@ -80,6 +81,15 @@ def getTime( opt='D' ):
     else:
         return date_time.strftime('%d-%H_%M')
 
+def closeFiles():
+    try:
+        file_start.close()
+        file_testbench.close()
+        file_end.close()
+    except Exception as e:
+        print(f"Error closing files: {e} \u2717")
+        raise
+
 
 
 def getMetrics( file_temp, time_of_test ):
@@ -131,9 +141,8 @@ def saveInFile( input ):
 
 try:
 
-
     # Default values
-    time_test = 3
+    time_test = 30
     control_time = 3
     com_port = 'COM3'
     bps = 115200
@@ -199,6 +208,8 @@ try:
         print("\tFiles opened. \u2713")
     except Exception as e:
         print(f"Error opening files: {e} \u2717")
+        raise
+
 
 
     saveInFile(info)
@@ -210,6 +221,7 @@ try:
         arduinoSerial       = serial.Serial(com_port , bps)
     except Exception as e:
         print(f"Error opening serial connection with Arduino: {e} \u2717")
+        raise
 
     sensitivity         = 0.068
     adjust_intensity    = 0.0 
@@ -220,38 +232,38 @@ try:
         getMetrics(file_start, int(control_time))    
     except Exception as e:
         print(f"Error during initial sampling: {e} \u2717")
+        raise
+
 
     try:
         cine = launchBench()
     except Exception as e:
         print(f"Error during launch testbench: {e} \u2717")
+        raise
+
 
     try:
         getMetrics(file_testbench, int(time_test))
     except Exception as e:
         print(f"Error during test sampling: {e} \u2717")
+        raise
+
 
     try:
         closeBench()
     except Exception as e:
         print(f"Error closing testbench: {e} \u2717")
+        raise
+
 
     try:
         getMetrics(file_end, int(control_time))
     except Exception as e:
         print(f"Error during final sampling: {e} \u2717")
+        raise
 
 
-
-    try:
-        file_start.close()
-        file_testbench.close()
-        file_end.close()
-    except Exception as e:
-        print(f"Error closing files: {e} \u2717")
-
-
-
+    closeFiles()
 
     #Open panda
     print("Read files CSV in panda")
@@ -262,6 +274,8 @@ try:
         print("\tFiles read successfully. \u2713")
     except Exception as e:
         print(f"Error reading CSV files: {e} \u2717")
+        raise
+
 
     #Concatenated in global file
     print("Joining files")
@@ -271,13 +285,14 @@ try:
         print("\tFiles concatenated. \u2713")
     except Exception as e:
         print(f"File concatenation error: {e}  \u2717")
+        raise
 
 
     try:
         global_testbench = pd.read_csv(path_global,  sep='\t', header=None, names=columns)
     except Exception as e:
         print(f"Error reading CSV files: {e} \u2717")
-
+        raise
 
 
     print("Applying filter")
@@ -288,6 +303,7 @@ try:
         print("\tFilter applied. \u2713")
     except Exception as e:
         print(f"File concatenation error: {e}  \u2717")
+        raise
 
 
     if (graphs == 'TRUE'):    
@@ -299,13 +315,25 @@ try:
             print("\tGraph with filter completed. \u2713")
         except Exception as e:
             print(f"Error making graphs: {e} \u2717")
+            raise
 
 
     try:
         arduinoSerial.close()
     except Exception as e:
         print(f"Error closing Arduino serial : {e} \u2717")
+        raise
+
 
     print("\nProcess completed successfully!!!\n")
-except Exception as e:
-    print(f"Error: {e} \u2717")
+except:
+    closeFiles()
+    file_info.close()
+    file_filter.close()
+    dir_rm = f"{device}/{nameGlobal}"
+    if (device == 'INTEL'):
+        subprocess.run(["powershell.exe", f'Remove-Item "{device}/{nameGlobal}" -Recurse -Confirm:$false'], shell=True)
+    else:
+        os.system(f"rm {device}/{nameGlobal}")
+    
+    print(f"Ejecution stoped \u2717")
